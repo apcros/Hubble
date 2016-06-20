d367af22547d3818df3aeee73350da3d7c8f3a9b
apcros <apcros.mail@gmail.com>
2016-06-20 21:50:37 +0100

    Added the base for the windows client

---
+++ Clients/Win/Hubble/Hubble/ApiEngine.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+namespace Hubble
+{
+ 
+  class ApiEngine
+    {
+        SystemInfoEngine sie; 
+        public ApiEngine()
+        {
+            sie = new SystemInfoEngine();
+        }
+
+        public void post()
+        {
+
+        }
+        public string getJson()
+        {
+            
+            List<Drive> drives = sie.getDrivesInfo();
+            HubbleJSON data = new HubbleJSON(drives.Count);
+            data.name = sie.getName();
+            data.os_version = sie.getOS();
+            data.ram_free = sie.getFreeRam();
+            data.ram_total = sie.getTotalRam();
+            data.cpu_usage = sie.getCpuUsage();
+            data.client_version = "ALPHA - 0.1 - WIN32";
+            
+            int i = 0;
+            foreach(Drive drive in drives)
+            {
+                data.drives[i] = drive;
+                i++;
+            }
+            JavaScriptSerializer jss = new JavaScriptSerializer();
+                return jss.Serialize(data);
+        }
+    }
+
+    class HubbleJSON
+    {
+        public string name;
+        public string os_version;
+        public string client_version;
+        public string ram_total;
+        public string ram_free;
+        public string cpu_usage;
+        public Drive[] drives;
+        public HubbleJSON (int sizeDrives) {
+                drives = new Drive[sizeDrives];
+            }
+    }
+}
---
+++ Clients/Win/Hubble/Hubble/SystemInfoEngine.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hubble
+{
+    class SystemInfoEngine
+    {
+        private PerformanceCounter cpuCounter;
+        public SystemInfoEngine()
+        {
+            cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+        }
+
+        public string getTotalRam()
+        {
+            ulong ramTotal = new Microsoft.VisualBasic.Devices.ComputerInfo().TotalPhysicalMemory;
+            ramTotal =  ramTotal / 1024 / 1024;
+            return Convert.ToString(ramTotal);
+        }
+
+        public string getFreeRam()
+        {
+            ulong ramFree = new Microsoft.VisualBasic.Devices.ComputerInfo().AvailablePhysicalMemory;
+            ramFree = ramFree / 1024 / 1024;
+            return Convert.ToString(ramFree);
+
+        }
+
+        public string getCpuUsage()
+        {
+            
+            return Convert.ToString(Math.Round(cpuCounter.NextValue()));
+        }
+
+        public string getName()
+        {
+            return Environment.MachineName; 
+        }
+
+        public string getOS()
+        {
+            return Convert.ToString(Environment.OSVersion);
+        }
+        public List<Drive> getDrivesInfo()
+        {
+        
+            System.IO.DriveInfo[] drives = System.IO.DriveInfo.GetDrives();
+            var list = new List<Drive>();
+
+            foreach (System.IO.DriveInfo drive in drives)
+            {
+                if (drive.IsReady)
+                {
+                    list.Add(new Drive()
+                    {
+                        name = drive.Name,
+                        free_space = Convert.ToString(drive.AvailableFreeSpace / 1024 / 1024),
+                        total_space = Convert.ToString(drive.TotalSize / 1024 / 1024),
+                        label = drive.VolumeLabel,
+                        type = Convert.ToString(drive.DriveType),
+                        format = drive.DriveFormat
+                    });
+                }
+            }
+            return list;
+        }
+
+
+    }
+
+    class Drive
+    {
+        public string format;
+        public string free_space;
+        public string total_space;
+        public string name;
+        public string label;
+        public string type;
+    }
+    
+}
