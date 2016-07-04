ebf8372e00fcea3645a17f6ad038e592f822f108
apcros <apcros.mail@gmail.com>
2016-07-04 20:35:00 +0100

    Added test wireframe

--- Server/hubble/tests/DeviceTest.php
+++ Server/hubble/tests/DeviceTest.php
@@ -7,4 +7,21 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 class DeviceTest extends TestCase
 {
 
+
+    public function testAddDevice() {
+
+    }
+
+    public function testRemoveDevice() {
+
+    }
+
+    public function testUpdateDevice() {
+
+    }
+
+    public function testFetchDevice() {
+
+    }
+    
 }
