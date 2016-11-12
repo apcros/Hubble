c023ef28cb5596c43f3ba92a79df5ef0102047a2
Apcros <apcros.mail@gmail.com>
2016-11-12 02:15:25 +0000

    Added healthcheck and better versionning

--- hubble/app/Http/routes.php
+++ hubble/app/Http/routes.php
@@ -9,6 +9,7 @@
 Route::post('/api/v1/devices/{device_uuid}/latest', 'Device@updateData');
 Route::get('/api/v1/devices/{device_uuid}/latest', 'Device@showData');
 Route::get('/api/v1/devices/list', 'Device@listDevicesRaw');
+Route::get('/api/v1/healthcheck', 'Api@healthcheck');
 
 Route::get('/ajax/devices/list','Device@listDevices');
 Route::post('/ajax/devices/add','Device@add');
